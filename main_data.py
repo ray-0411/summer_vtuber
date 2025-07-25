0b68b5267206f98841568410e6917a7deca9747d
ray-0411 <ray0411ray@gmail.com>
2025-07-25 17:42:34 +0800

    main part2 選單

--- main_data.py
+++ main_data.py
@@ -7,13 +7,25 @@ db_path = "data.db"
 with sqlite3.connect(db_path) as conn:
     # 讀取所有資料
     df = pd.read_sql_query("SELECT * FROM main", conn)
+    df_streamer = pd.read_sql_query("SELECT * FROM streamer", conn)
+
+
 
 # 轉成時間戳記方便排序
 df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
 
-# 建立頻道選單
-channels = sorted(df['channel'].unique())
-selected_channel = st.selectbox("請選擇頻道", channels)
+
+# 根據 streamer.channel_id 排序，只留下在 main 表出現過的頻道
+streamer_channels = df_streamer['channel_id'].tolist()
+channels_in_data = df['channel'].unique()
+channels = [ch for ch in streamer_channels if ch in channels_in_data]
+
+# 建立頻道名稱與 ID 的對應字典
+channel_dict = df_streamer.set_index('channel_name')['channel_id'].to_dict()
+
+# 中文選單
+selected_name = st.selectbox("請選擇頻道", list(channel_dict.keys()))
+selected_channel = channel_dict[selected_name]  # 對應 channel_id
 
 # 篩選出該頻道的所有紀錄
 df_selected = df[df['channel'] == selected_channel].copy()
